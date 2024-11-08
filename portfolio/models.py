350805ad50616f3aa76a2b293ae72591b49267ed
Grégory Eyraud <gregory.eyraud@gmail.com>
2024-11-08 11:26:04 +0100

    Création d'une base de données PostgreSQL et des modèles

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+# portfolio/models.py
+from django.db import models
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+    contact_email = models.EmailField()
+    linkedin = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
