b8adfe26d4beae863007689b6a00ce4ccad80cd4
Grégory Eyraud <gregory.eyraud@gmail.com>
2024-12-01 19:07:43 +0100

    Ajout de Withenoise pour les fichiers statiques

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -68,6 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Ajoutez cette ligne
 ]
 
 ROOT_URLCONF = 'myportfolio.urls'
@@ -165,7 +166,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'gregory.eyraud@gmail.com'  # Adresse email utilisée pour envoyer
-EMAIL_HOST_PASSWORD = 'lnjf jyty tjpq syws'  # Mot de passe de cette adresse
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # Adresse email utilisée pour envoyer
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Mot de passe de cette adresse
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
